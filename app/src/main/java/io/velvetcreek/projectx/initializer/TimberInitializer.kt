d7456f6795f972c73687887bc3d59574b9f62aa2
andys-hub <64394640+andys-hub@users.noreply.github.com>
2022-01-25 21:55:40 -0800

    Add Timber logger

---
+++ app/src/main/java/io/velvetcreek/projectx/initializer/TimberInitializer.kt
@@ -0,0 +1,18 @@
+package io.velvetcreek.projectx.initializer
+
+import android.content.Context
+import androidx.startup.Initializer
+import io.velvetcreek.projectx.BuildConfig
+import timber.log.Timber
+
+class TimberInitializer : Initializer<Unit> {
+
+    override fun create(context: Context) {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+            Timber.d("TimberInitializer is initialized.")
+        }
+    }
+
+    override fun dependencies(): List<Class<out Initializer<*>>> = emptyList()
+}
